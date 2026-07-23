101cf7740036bdd94a1582e50a03f0d227c7641a
Your Name <your_email@example.com>
2026-07-24 02:32:22 +0600

    Label changed

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $error = "";
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Sonic</title>
+    <title>ARN QuickFIx</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
